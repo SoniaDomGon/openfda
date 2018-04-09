1eae40ae998b129687a1b54f3fdc770b81cd6efa
soniadg <soniadg99@gmail.com>
2018-04-09 15:09:50 +0200

    Este es el programa 2 de la práctica 1

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?&limit=1", None, headers)
+conn.request("GET", "/drug/label.json?&limit=10", None, headers)
 inf = conn.getresponse()
 print(inf.status, inf.reason)
 prods_raw = inf.read().decode("utf-8")
@@ -15,5 +15,5 @@ prods = json.loads(prods_raw)
 obj = 0
 while obj < 10:
     prod = prods['results'][obj]
-    print("id: ", drug['id'])
+    print("id: ", prod['id'])
     obj +=1
